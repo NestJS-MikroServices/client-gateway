0e7cf7e522841275f0b1516ff5647bb9174f1538
Louis Rey <rlhardrock@gmail.com>
2024-10-31 20:47:19 -0500

    Client-gateway FindAll setting, due to pagination Dto destructuring, did not work directly

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,23 @@
-import {Body, Controller, Delete, Get, Inject, Param, Patch, Post} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  Query
+} from '@nestjs/common';
 import {PRODUCT_SERVICE} from "../config";
-import {ClientProxy} from "@nestjs/microservices";
+import {ClientProxy} from '@nestjs/microservices';
+import {PaginationDto} from '../common';
 
 
 @Controller('products')
 export class ProductsController {
   constructor(
-    @Inject(PRODUCT_SERVICE) private readonly ProductsClient: ClientProxy,
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
   ) {}
 
   @Post()
@@ -15,8 +26,9 @@ export class ProductsController {
   }
 
   @Get()
-  findAllProducts(){
-    return this.ProductsClient.send({ cmd: 'find_all_products' }, {})
+  findAllProducts( @Query() paginationDto: PaginationDto ){
+    const { page, limit } = paginationDto;
+    return this.productsClient.send({ cmd: 'find_all_products' }, { page, limit })
   }
 
   @Get(':id')
